e5aae7c0fc943cc0d2ee4f9ceaacf09ee9cc5450
Volker Runkel <vrunkel@me.com>
2026-03-12 07:56:11 +0100

    Moved BatSoundHandling to its own repo for use with package manager

---
+++ BATMLTool/BATMLTool/BATMLToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  BATMLToolApp.swift
+//  BATMLTool
+//
+//  Created by Volker Runkel on 12.03.26.
+//
+
+import SwiftUI
+
+@main
+struct BATMLToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BATMLTool/BATMLTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BATMLTool
+//
+//  Created by Volker Runkel on 12.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
